75e25f59e02c3de786fef9ff252a9b54c47eec6e
John Carbajal <johnxjuanito@gmail.com>
2019-10-17 08:45:20 -0700

    final

---
+++ 1.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("#add").on("click", function(e) {
+    $("#ansr").val(Number($("#num_one").val()) + Number($("#num_two").val()))
+  })
+  $("#subtract").on("click", function(e) {
+    $("#ansr").val(Number($("#num_one").val()) - Number($("#num_two").val()))
+  })
+  $("#multiply").on("click", function(e) {
+    $("#ansr").val(Number($("#num_one").val()) * Number($("#num_two").val()))
+  })
+  $("#divide").on("click", function(e) {
+    $("#ansr").val(Number($("#num_one").val()) / Number($("#num_two").val()))
+  })
+})
